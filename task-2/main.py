11aa95b430aab8fe57847950baf9c429137d1502
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2024-12-24 04:00:47 +0200

    remove whitespace

--- task-2/main.py
+++ task-2/main.py
@@ -1,12 +1,12 @@
-#  Task 2
-#  * Prepare promise based alternative
-#  * Write use cases for the promise based solution
-#  * Write use cases for the async-await
-#  * Add new on-demend feature during review
-#    e.g.: Add support for parallelism
+# Task 2
+# * Prepare promise based alternative
+# * Write use cases for the promise based solution
+# * Write use cases for the async-await
+# * Add new on-demend feature during review
+#   e.g.: Add support for parallelism
 #
-#  Note: for technologies that do not have the native Future-like async functionalities
-#  You may combine Task 1 and 2 into a single Task that will showcase the idiomatic way of handling concurrency.
+# Note: for technologies that do not have the native Future-like async functionalities
+# You may combine Task 1 and 2 into a single Task that will showcase the idiomatic way of handling concurrency.
 
 from random import random
 from time import perf_counter
