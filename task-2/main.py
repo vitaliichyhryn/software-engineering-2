7c6162d1b25fcb7baec9d85f04970e280c9d0a14
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2024-12-25 00:20:53 +0200

    complete task 2

--- task-2/main.py
+++ task-2/main.py
@@ -8,48 +8,76 @@
 # Note: for technologies that do not have the native Future-like async functionalities
 # You may combine Task 1 and 2 into a single Task that will showcase the idiomatic way of handling concurrency.
 
-from random import random
-from time import perf_counter
-from multiprocessing import Pool
+import asyncio, functools
 from typing import Any, Callable, Iterable, List 
+from random import randint
+
+responses = {
+    "foo.com": {"message": "Hello from foo.com!"},
+    "bar.com": {"message": "Hello from bar.com!"},
+    "baz.com": {"message": "Hello from baz.com!"},
+}
 
 
-def map_parallel(
-    func: Callable,
-    iter: Iterable[Any],
-    *iters: Iterable[Any],
+def debounce(
+    timeframe: float,
+) -> Callable:
+    def wrapper(func: Callable) -> Callable:
+        lock = asyncio.Lock()
+        called: float | None = None
+
+        @functools.wraps(func)
+        async def wrapped(*args, **kwargs) -> Any:
+            nonlocal called, lock
+            loop = asyncio.get_event_loop()
+            async with lock:
+                if called:
+                    now = loop.time()
+                    elapsed = now - called
+
+                    if elapsed < timeframe:
+                        left = timeframe - elapsed
+                        await asyncio.sleep(left)
+                        print(f"Slept for {left} seconds.")
+
+            called = loop.time()
+            return await func(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+
+async def map_async(
+    function: Callable,
+    iterable: Iterable[Any],
 ) -> List[Any]:
-    results = []
-    param_iters = zip(iter, *iters)
-    with Pool() as pool:
-        results = pool.starmap(func, param_iters)
-    return results
+    awaits = [function(item) for item in iterable]
+    tasks = asyncio.gather(*awaits)
+    result = await tasks
+    return result
 
 
-def monte_carlo_method(n: int) -> float:
-    points_inside = 0
-    for _ in range(n):
-        (x, y) = (random(), random())
-        distance = x * x + y * y
-        if distance <= 1:
-            points_inside += 1
-    return 4 * (points_inside / n)
+@debounce(timeframe=3)
+async def send_request(
+        endpoint
+) -> dict[str, str] | None:
+    delay = randint(0, 5)
+    await asyncio.sleep(delay)
+    print(f"Request to {endpoint} took {delay} seconds to complete.")
+    response: dict[str, str] | None = responses.get(endpoint)
+    if response:
+        message: str | None = response.get("message")
+        if message:
+            print(f"Got message: {message}")
+    return response
 
 
-def main() -> None:
-    ns = [10 ** 6 for i in range(10)]
-
-    start = perf_counter()
-    results = map_parallel(monte_carlo_method, ns)
-    mean = sum(results) / len(results)
-    print(f"Approximate value of pi: {mean}")
-    print(f"map_parallel execution time: {perf_counter() - start} seconds")
-
-    start = perf_counter()
-    results = list(map(monte_carlo_method, ns))
-    mean = sum(results) / len(results)
-    print(f"approx value of pi: {mean}")
-    print(f"map execution time: {perf_counter() - start} seconds")
+async def main() -> None:
+    endpoints = responses.keys()
+    task = asyncio.create_task(
+        map_async(send_request, endpoints)
+    )
+    result = await task
+    print(result)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main()) 
