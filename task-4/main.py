958f2b09fe034deccb70110525f68f003d4b98fb
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2024-12-25 22:53:58 +0200

    add type hints to task 4

--- task-4/main.py
+++ task-4/main.py
@@ -3,18 +3,21 @@
 import asyncio
 import hashlib
 from random import randint
+from typing import AsyncGenerator, List
 
 
-async def stream(source):
+async def stream_str(
+        source: List[str]
+) -> AsyncGenerator[str]:
     for chunk in source:
         delay = randint(0, 3)
         await asyncio.sleep(delay)
         yield chunk
 
 
-async def get_sha256(
-    stream
-):
+async def get_str_sha256(
+    stream: AsyncGenerator
+) -> str:
     hasher = hashlib.sha256()
     async for chunk in stream:
         print(f"Got chunk: {chunk}")
@@ -22,19 +25,18 @@ async def get_sha256(
     return hasher.hexdigest()
 
 
-async def main():
-    chunked_string = [
-        "Some ",
-        "important ",
+async def main() -> None:
+    important_data = [
+        "Important ",
         "data.",
     ]
-    other_chunked_string = [
-        "Hash ",
-        "this.",
+    complex_password = [
+        "Complex ",
+        "password."
     ]
     task = asyncio.gather(
-        get_sha256(stream(chunked_string)),
-        get_sha256(stream(other_chunked_string))
+        get_str_sha256(stream_str(important_data)),
+        get_str_sha256(stream_str(complex_password))
     )
     result = await task
     print(result)
