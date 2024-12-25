3c48d10c45099db027c7fc88971eb0aae27a954e
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2024-12-25 20:25:34 +0200

    complete task 3

--- task-3/main.py
+++ task-3/main.py
@@ -1,3 +1,41 @@
 # Task 3
-# * Integrate AbortController or other Cancallable approach
+# * Integrate AbortController or other Cancelable approach
 
+import asyncio
+
+
+async def get_hourly_forecast():
+    try:
+        file_name = "hourly1h_zip_eu.json.gz"
+        api_key = "some_api_key"
+        url = f"https://bulk.openweathermap.org/snapshot/{file_name}?appid={api_key}"
+        delay = 5
+        await asyncio.sleep(delay)
+        print("Finished downloading the forecast.")
+        forecast = {
+            "country": "UA",
+            "city": "Kyiv",
+            "temperature": "0",
+            "datetime": "2024-25-12 20:00:00"
+        }
+        return forecast
+    except asyncio.CancelledError:
+        print("The forecast download was cancelled.")
+        raise
+
+
+async def main():
+    task = asyncio.create_task(get_hourly_forecast())
+
+    delay = 3
+    await asyncio.sleep(delay)
+    task.cancel()
+
+    try:
+        result = await task
+        print(result)
+    except asyncio.CancelledError:
+        print("The task was cancelled.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
