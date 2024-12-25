21c0535d10ce89fcba4fa11d25e1af3edc1866fb
Chyhryn Vitalii <chyhryn.vitalii@gmail.com>
2024-12-26 00:30:28 +0200

    complete task 5

---
+++ task-5/main.py
@@ -0,0 +1,53 @@
+# Task 5 (Observable/EventEmitter/Alternative) -- Reactive message based communication between entities
+
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Observer(ABC):
+    @abstractmethod
+    def update(self, *args, **kwargs):
+        pass
+
+
+class Observable(ABC):
+    def __init__(self) -> None:
+        self.observers: List[Observer]= []
+
+    def attach(self, observer: Observer):
+        self.observers.append(observer)
+
+    def notify(self, *args, **kwargs):
+        for observer in self.observers:
+            observer.update(*args, **kwargs)
+
+
+class User(Observable):
+    def __init__(self, username: str, password: str) -> None:
+        super().__init__()
+        self.username: str = username
+        self._password: str = password
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        self._password = value
+        self.notify(self.username)
+
+
+class UserLogger(Observer):
+    def update(self, username: str):
+        print(f"User '{username}' changed their password.")
+
+
+def main():
+    john = User("johndoe", "pass")
+    logger = UserLogger()
+    john.attach(logger)
+    john.password = "#E2aWW@4v%2^&P&S"
+
+if __name__ == "__main__":
+    main()
